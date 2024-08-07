e50c0f04306d8ef1322eb296ee57d0eb86ee9e3f
nt-tsuji <tsuji@phys.s.u-tokyo.ac.jp>
2024-08-07 22:58:46 +0900

    Add files via upload

--- src/main.jl
+++ src/main.jl
@@ -1,19 +1,19 @@
 include("time-dep-BCS.jl")
 
-Nt = 1000;
-tmax = 50.;
-Vi = 2.;
-Vf = 6.;
-t_hop = 1.;
-T = 0.05;
-Nk = 200;
-tolerance = 0.000001;
+Nt = 1000
+tmax = 50.0
+Vi = 2.0
+Vf = 6.0
+t_hop = 1.0
+T = 0.05
+Nk = 200
+tolerance = 0.000001
 
-param = Param(Nt, tmax, Vi, Vf, t_hop, T, Nk, tolerance);
-output = Output(param.Nt);
-time_dep_BCS!(param, output);
+param = Param(Nt, tmax, Vi, Vf, t_hop, T, Nk, tolerance)
+output = Output(param.Nt)
+time_dep_BCS!(param, output)
 
-fig_Delta = plot(output.t_list, [output.Δ_re, output.Δ_im], xlim=(0,param.tmax), ylim=(0,4), xlabel="t", label=["Re Δ" "Im Δ"])
+fig_Delta = plot(output.t_list, [real(output.Δ), imag(output.Δ)], xlim=(0,param.tmax), ylim=(0,4), xlabel="t", label=["Re Δ" "Im Δ"])
 fig_E_tot = plot(output.t_list, output.E_tot, xlim=(0,param.tmax), ylim=(-1.5,-1.3), xlabel="t", label="E_tot")
 
 savefig(fig_Delta,"Delta.png")
--- src/time-dep-BCS.jl
+++ src/time-dep-BCS.jl
@@ -12,76 +12,74 @@ struct Param
     tolerance::Float64  # tolerance of convergence
     dk::Float64         # dk = 2π/Nk
     dt::Float64         # dt = tmax/Nt
-    Param(Nt, tmax, Vi, Vf, t_hop, T, Nk, tolerance) = new(Nt, tmax, Vi, Vf, t_hop, T, Nk, tolerance, 2*π/Nk, tmax/Nt)
+    Param(Nt, tmax, Vi, Vf, t_hop, T, Nk, tolerance) = new(Nt, tmax, Vi, Vf, t_hop, T, Nk, tolerance, 2.0*π/Nk, tmax/Nt)
 end
 
 # output data
 mutable struct Output
     t_list::Array{Float64,1}   # list of time steps
-    Δ_re::Array{Float64,1}     # real part of the gap function
-    Δ_im::Array{Float64,1}     # imaginary part of the gap function
+    Δ::Array{ComplexF64,1}     # gap function
     E_tot::Array{Float64,1}    # total energy
     iter_list::Array{Int16,1}  # list of numbers of iterations
-    Output(Nt::Int64) = new(zeros(Float64,Nt+1), zeros(Float64,Nt+1), zeros(Float64,Nt+1), zeros(Float64,Nt+1), zeros(Int64,Nt+1))
+    Output(Nt::Int64) = new(zeros(Float64,Nt+1), zeros(ComplexF64,Nt+1), zeros(Float64,Nt+1), zeros(Int64,Nt+1))
 end
 
 function time_dep_BCS!(param::Param, output::Output)
     # gap function
     Δ = zeros(ComplexF64, param.Nt+1)
-    Δ[1] = 1.
+    Δ[1] = 1.0
 
     # pseudospin
     σk = zeros(Float64, 3, param.Nk, param.Nt+1)
 
     # Pauli matrices
-    τ1 = [0.  1.; 1.  0.]
-    τ2 = [0. -im; im  0.]
-    τ3 = [1.  0.; 0. -1.]
+    τ1 = [0.0 1.0; 1.0 0.0]
+    τ2 = [0.0 -im; im  0.0]
+    τ3 = [1.0 0.0; 0. -1.0]
     τ = [τ1, τ2, τ3]
     
     # BdG Hamiltonian
     function hk(k::Float64, Δ::ComplexF64)
-        return -2*param.t_hop*cos(k)*τ3 - Δ*(τ1-im*τ2)/2 - conj(Δ)*(τ1+im*τ2)/2
+        return -2.0*param.t_hop*cos(k)*τ3 - Δ*(τ1-im*τ2)/2.0 - conj(Δ)*(τ1+im*τ2)/2.0
     end
 
     # Effective magnetic field
     function bk(k::Float64, Δ::ComplexF64)
-        return [-real(Δ), -imag(Δ), -2*param.t_hop*cos(k)]
+        return [-real(Δ), -imag(Δ), -2.0*param.t_hop*cos(k)]
     end
 
     # equilibrium mean-field self-consistency
-    diff = 1.
+    diff = 1.0
     iter = 0
     while diff > param.tolerance
-        Δ_temp = 0.
+        Δ_temp = 0.0
         for ik in 1:param.Nk
             k = ik*param.dk
             Ek,Uk = eigen(hk(k,Δ[1]))
-            f = diagm([1/(exp(Ek[1]/param.T)+1.0), 1/(exp(Ek[2]/param.T)+1.0)])
-            Δ_temp += tr(f*Uk'*(τ1+im*τ2)/2*Uk)
+            f = diagm([1.0/(exp(Ek[1]/param.T)+1.0), 1.0/(exp(Ek[2]/param.T)+1.0)])
+            Δ_temp += tr(f*Uk'*(τ1+im*τ2)/2.0*Uk)
         end
         Δ_temp = Δ_temp*param.Vi/param.Nk
         diff = abs(Δ[1]-Δ_temp)
         Δ[1] = Δ_temp
         iter += 1
     end
-    output.t_list[1] = 0.
-    output.Δ_re[1] = real(Δ[1])
-    output.Δ_im[1] = imag(Δ[1])
+    output.t_list[1] = 0.0
+    output.Δ[1] = Δ[1]
     output.iter_list[1] = iter
 
     # initial pseudospin configuration
-    E_tot = 0.
+    E_tot = 0.0
     for ik in 1:param.Nk
         k = ik*param.dk;
         Ek,Uk = eigen(hk(k,Δ[1]))
-        f = diagm([1/(exp(Ek[1]/param.T)+1.0), 1/(exp(Ek[2]/param.T)+1.0)])
+        f = diagm([1.0/(exp(Ek[1]/param.T)+1.0), 1.0/(exp(Ek[2]/param.T)+1.0)])
         for α in 1:3
-            σk[α,ik,1] = real(tr(f*Uk'*τ[α]*Uk)/2)
+            σk[α,ik,1] = real(tr(f*Uk'*τ[α]*Uk)/2.0)
         end
         E_tot += bk(k,Δ[1])⋅σk[:,ik,1]
     end
-    E_tot = 2*E_tot[1]/param.Nk + abs2(Δ[1])/param.Vi
+    E_tot = 2.0*E_tot[1]/param.Nk + abs2(Δ[1])/param.Vi
     output.E_tot[1] = E_tot
 
     # time evolution
@@ -89,10 +87,10 @@ function time_dep_BCS!(param::Param, output::Output)
         t = it*param.dt
         Δ[it+1] = Δ[it]
         σk[:,:,it+1] = σk[:,:,it]
-        diff = 1.
+        diff = 1.0
         iter = 0
         while diff > param.tolerance
-            Δ_temp = 0.
+            Δ_temp = 0.0
             for ik in 1:param.Nk
                 k = ik*param.dk
                 σk[:,ik,it+1] = σk[:,ik,it] + param.dt*(bk(k,Δ[it])×σk[:,ik,it]+bk(k,Δ[it+1])×σk[:,ik,it+1])
@@ -104,15 +102,14 @@ function time_dep_BCS!(param::Param, output::Output)
             iter += 1
         end
         output.t_list[it+1] = t
-        output.Δ_re[it+1] = real(Δ[it+1])
-        output.Δ_im[it+1] = imag(Δ[it+1])
+        output.Δ[it+1] = Δ[it+1]
         output.iter_list[it+1] = iter
-        E_tot = 0.
+        E_tot = 0.0
         for ik in 1:param.Nk
             k = ik*param.dk;
             E_tot += bk(k,Δ[it+1])⋅σk[:,ik,it+1]
         end
-        E_tot = 2*E_tot/param.Nk + abs2(Δ[it+1])/param.Vf
+        E_tot = 2.0*E_tot/param.Nk + abs2(Δ[it+1])/param.Vf
         output.E_tot[it+1] = E_tot
     end
 end
