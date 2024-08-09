1ca3d32b7c55a132a7609c989e8dec131de07f14
nt-tsuji <tsuji@phys.s.u-tokyo.ac.jp>
2024-08-09 10:15:09 +0900

    Add files via upload

--- src/main.jl
+++ src/main.jl
@@ -18,7 +18,7 @@ time_dep_BCS_RK2!(param, output)
 # time_dep_BCS_RK4!(param, output)
 
 fig_Delta = plot(output.t_list, [real(output.Δ), imag(output.Δ)], xlim=(0,param.tmax), ylim=(0,4), xlabel="t", label=["Re Δ" "Im Δ"])
-fig_E_tot = plot(output.t_list, output.E_tot, xlim=(0,param.tmax), ylim=(-1.5,-1.3), xlabel="t", label="E_tot")
+fig_E_tot = plot(output.t_list, output.E_tot, xlim=(0,param.tmax), ylim=(-1.4,-1.395), xlabel="t", label="E_tot")
 
 savefig(fig_Delta,"Delta.png")
 savefig(fig_E_tot,"E_tot.png")
